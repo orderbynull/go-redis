dd9b6ac14a9e7729772e1825b9fb116df2665d34
Dmitry Patsura <zaets28rus@gmail.com>
2016-02-22 16:52:38 +0300

    Implement PING command

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"github.com/labstack/gommon/log"
+"bufio"
+"bytes"
 )
 
 const (
@@ -44,11 +46,40 @@ func handleRequest(conn net.Conn) {
 		fmt.Println("Error reading:", err.Error())
 	}
 
-	log.Println("Rec ", string(buf))
 
-	if (string(buf) == "SET") {
-		conn.Write([]byte("+PONG"))
+	//readbuffer := bytes.NewBuffer([]byte("123\r\n456"))
+	//reader := bufio.NewReader(readbuffer)
+
+	bufferForReader := bytes.NewBuffer(buf)
+	bufReader := bufio.NewReader(bufferForReader)
+
+	read:
+	line, isPrefis, readErr := bufReader.ReadLine();
+	for  readErr == nil {
+		log.Println("Request line ", string(line))
+		log.Println(isPrefis)
+
+		//log.Print("PING OPERATROR %s", string(line) == "PING")
+		if (string(line) == "PING") {
+			writer := bufio.NewWriter(conn)
+			writer.WriteString("+PONG\r\n")
+			writer.Flush()
+		} else if (string(line) == "PONG") {
+			writer := bufio.NewWriter(conn)
+			writer.WriteString("+PING\r\n")
+			writer.Flush()
+		} else {
+			writer := bufio.NewWriter(conn)
+			writer.WriteString("+OK\r\n")
+			writer.Flush()
+		}
+
+		goto read
 	}
+
+	//numbs, responseWriteError := conn.Write([]byte("+PONG"))
+	//log.Print(numbs, responseWriteError)
+
 
 	// Close the connection when you're done with it.
 	//conn.Close()
